f6ea63018ed6410e10c28fecd9c9b808b948284e
andrewjlawrence <ajlawrence@acm.org>
2016-04-06 18:33:25 +0100

    Fixed benchmark script
    Fixed benchmark script

--- benchmarks/runbenchmarks.py
+++ benchmarks/runbenchmarks.py
@@ -1,32 +1,34 @@
 ### Run benchmarks ###
 import subprocess
 import sys
-import threading
-from threading import Timer
+from threading import Timer,Thread
 
 def run(param, timeout_sec):
-  proc = subprocess.Popen(param, stdout=subprocess.PIPE, 
-    stderr=subprocess.PIPE)
-  kill_proc = lambda p: p.kill()
-  timer = Timer(timeout_sec, kill_proc, [proc])
-  try:
-    timer.start()
-    stdout,stderr = proc.communicate()
-    print("benchmark: " + param[0])
-    print(stdout.decode())
-  finally:
-    timer.cancel()
+    print("Starting thread")
+    proc = subprocess.Popen(param, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    kill_proc = lambda p: p.kill()
+    timer = Timer(timeout_sec, kill_proc, [proc])
+    try:
+        timer.start()
+        stdout, stderr = proc.communicate()
+        print("benchmark: " + param[0])
+        print(stdout.decode())
+    finally:
+        timer.cancel()
 
 solverpath = "../Debug/solver.exe"
 timeout = 10000
 benchmarklist = ["quinn.cnf",
-                 #"simplest.cnf",
-                 #"test.cnf",
+                 # "simplest.cnf",
+                 # "test.cnf",
                  "PHOLE-UNSAT/hole6.cnf",
                  "AIM-3SAT-MIXED/aim-50-1_6-no-1.cnf",
                  "AIM-3SAT-MIXED/aim-50-1_6-yes1-1.cnf"
-                ]
+                 ]
 paramlist = [[solverpath, "-f", x] for x in benchmarklist]
-for param in paramlist:
-	threading.start_new_thread(run,(param, timeout))
-	
+if __name__ == "__main__":
+    for param in paramlist:
+        thread = Thread(target = run, args = (param, timeout))
+        thread.start()
+    print("Finished script")
