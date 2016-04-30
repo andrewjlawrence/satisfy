3eccb5d2fdcfedda93c17a496dcf57c07a297004
andrewjlawrence <ajlawrence@acm.org>
2016-04-30 13:44:33 +0100

    Added heuristic.h

---
+++ lib/sat/inc/heuristic.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2016 Andrew Lawrence
+ */
+#ifndef __HEURISTIC_H
+#define __HEURISTIC_H
+
+// Project includes
+#include <sat-types.h>
+
+namespace SAT
+{
+class Heuristic
+{
+
+};
+
+} // End namespace SAT
+#endif // __HEURISTIC_H
